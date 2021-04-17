2d1284cedffce962cd98488a835fd57217789ff4
zakaria-root <elmourtazakezia@gmail.com>
2021-04-17 09:03:25 +0000

    ajouter un chier policy

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,6 +21,11 @@
 
       @yield('content')
     </div>
+    <!-- MDB -->
+<script
+type="text/javascript"
+src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.js"
+></script>
 </body>
 </html>
 
